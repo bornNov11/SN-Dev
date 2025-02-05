516bd762d241a45143aa6acd70581048fb56e61a
bornNov11 <lsy882692@gmail.com>
2025-02-06 07:14:07 +0900

    analytics link

---
+++ js/music-list.js
@@ -0,0 +1,44 @@
+let allMusic = [
+    {
+        name: "Just the Two of Us",
+        artist: "Grover Washington Jr",
+        img: "alone",
+        src: "music-1"
+    },
+    {
+        name: "Kimiga Door O Shimeta Ato",
+        artist: "backnumber",
+        img: "gizigae",
+        src: "music-2"
+    },
+    {
+        name: "Suiheisen",
+        artist: "backnumber",
+        img: "fish",
+        src: "music-3"
+    },
+    {
+        name: "Kimi wa Rock wo Kikanai",
+        artist: "aimyon",
+        img: "pink",
+        src: "music-4"
+    },
+    {
+        name: "Hikari no Machi",
+        artist: "backnumber",
+        img: "cafe",
+        src: "music-5"
+    },
+    {
+        name: "I'd Rather Pretend",
+        artist: "Bryant Barnes",
+        img: "rain",
+        src: "music-6"
+    },
+    {
+        name: "인사",
+        artist: "범진",
+        img: "window",
+        src: "music-7"
+    }
+]
